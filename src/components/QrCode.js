891b64057286383ff5f0746c17b457796dd9820e
Nino Verhaegh <454777@student.fontys.nl>
2022-01-17 14:45:08 +0100

    change url

--- src/components/QrCode.js
+++ src/components/QrCode.js
@@ -3,7 +3,7 @@ import QRCode from "qrcode.react";
 
 export default class Qrcode extends React.Component {
   state = {
-    base: "http://localhost:3000/table/?key=",
+    base: "https://24eat-lime.vercel.app/?key=",
   };
 
   render() {
@@ -16,20 +16,10 @@ export default class Qrcode extends React.Component {
         <div>
           <QRCode
             value={full}
-            size={128}
-            bgColor={"#ffffff"}
-            fgColor={"#0008ff"}
+            size={130}
             level={"H"}
             includeMargin={true}
             renderAs={"canvas"}
-            imageSettings={{
-              src: "https://www.lovemysalad.com/sites/default/files/styles/image_530x397/public/banaan-large.jpg?itok=dZxH6D_w",
-              x: null,
-              y: null,
-              height: 30,
-              width: 30,
-              excavate: true,
-            }}
           />
         </div>
       );
